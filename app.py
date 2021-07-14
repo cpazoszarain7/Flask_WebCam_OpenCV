b360693eb434061cac1dbddcc1945399ad89e12f
Carlos Pazos <cpazoszarain@gmail.com>
2021-07-13 21:30:48 -0400

    added haarcascades

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import base64
 import io
 from imageio import imread
 import matplotlib.pyplot as plt
+import time
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(stdout))
@@ -19,18 +20,79 @@ app.config['DEBUG'] = True
 socketio = SocketIO(app)
 camera = Camera(Makeup_artist())
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+
+def detect(image):
+    '''
+    Function to detect faces/eyes and smiles in the image passed to this function
+    '''
+
+    # print(type(image))
+    # image = np.array(image.convert('RGB'))
+    
+    # Next two lines are for converting the image from 3 channel image (RGB) into 1 channel image
+    # img = cv2.cvtColor(new_img, 1)
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Passing grayscale image to perform detection
+    # We pass grayscaled image because opencv expects image with one channel
+    # Even if you don't convert the image into one channel, open-cv does it automatically.
+    # So, you can just comment line number 26 and 27.
+    # If you do, make sure that you change the variables name at appropriate places in the code below
+    # Don't blame me if you run into errors while doing that :P
+    
+    faces = face_cascade.detectMultiScale(image=image, scaleFactor=1.3, minNeighbors=5)
+    # The face_cascade classifier returns coordinates of the area in which the face might be located in the image
+    # These coordinates are (x,y,w,h)
+    # We will be looking for eyes and smile within this area instead of looking for them in the entire image
+    # This makes sense when you're looking for smiles and eyes in a face, if that is not your use case then
+    # you can pull the code segment out and make a different function for doing just that, specifically.
+
+
+    # Draw rectangle around faces
+    for (x, y, w, h) in faces:
+        
+        # The following are the parameters of cv2.rectangle()
+        # cv2.rectangle(image_to_draw_on, start_point, end_point, color, line_width)
+        cv2.rectangle(img=image, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
+        
+        roi = image[y:y+h, x:x+w]
+        
+        # Detecting eyes in the face(s) detected
+        eyes = eye_cascade.detectMultiScale(roi)
+        
+        # Detecting smiles in the face(s) detected
+        smile = smile_cascade.detectMultiScale(roi, minNeighbors = 25)
+        
+        # Drawing rectangle around eyes
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
+            
+        # Drawing rectangle around smile
+        for (sx,sy,sw,sh) in smile:
+            cv2.rectangle(roi, (sx, sy), (sx+sw, sy+sh), (0,0,255), 2)
+
+    # Returning the image with bounding boxes drawn on it (in case of detected objects), and faces array
+    return image, faces
 
 @socketio.on('input image', namespace='/test')
 def test_message(input):
     input = input.split(",")[1]
     camera.enqueue_input(input)
     image_data = input # Do your magical Image processing here!!
-    #image_data = image_data.decode("utf-8")
+    # image_data = image_data.decode("utf-8")
 
     img = imread(io.BytesIO(base64.b64decode(image_data)))
-    cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # cv2_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # cv2_img = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
+    # cv2_img=img
+    cv2_img, _ = detect(image=img)
+    time.sleep(0.01)
     cv2.imwrite("reconstructed.jpg", cv2_img)
     retval, buffer = cv2.imencode('.jpg', cv2_img)
+
     b = base64.b64encode(buffer)
     b = b.decode()
     image_data = "data:image/jpeg;base64," + b
