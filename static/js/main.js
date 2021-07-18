e70fa0ca2f03c7b45f44c243772a57d7a9a9b40f
Carlos Pazos <cpazoszarain@gmail.com>
2021-07-18 11:46:59 -0400

    camera switch test

--- static/js/main.js
+++ static/js/main.js
@@ -55,3 +55,167 @@ $(document).ready(function(){
   });
 });
 
+var switchCameraButton;
+var amountOfCameras = 0;
+var currentFacingMode = 'environment';
+
+// this function counts the amount of video inputs
+// it replaces DetectRTC that was previously implemented.
+function deviceCount() {
+  return new Promise(function (resolve) {
+    var videoInCount = 0;
+
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(function (devices) {
+        devices.forEach(function (device) {
+          if (device.kind === 'video') {
+            device.kind = 'videoinput';
+          }
+
+          if (device.kind === 'videoinput') {
+            videoInCount++;
+            console.log('videocam: ' + device.label);
+          }
+        });
+
+        resolve(videoInCount);
+      })
+      .catch(function (err) {
+        console.log(err.name + ': ' + err.message);
+        resolve(0);
+      });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  // check if mediaDevices is supported
+  if (
+    navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia &&
+    navigator.mediaDevices.enumerateDevices
+  ) {
+    // first we call getUserMedia to trigger permissions
+    // we need this before deviceCount, otherwise Safari doesn't return all the cameras
+    // we need to have the number in order to display the switch front/back button
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: true,
+      })
+      .then(function (stream) {
+        stream.getTracks().forEach(function (track) {
+          track.stop();
+        });
+
+        deviceCount().then(function (deviceCount) {
+          amountOfCameras = deviceCount;
+
+          // init the UI and the camera stream
+          initCameraUI();
+          // initCameraStream();
+        });
+      })
+      .catch(function (error) {
+        //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+        if (error === 'PermissionDeniedError') {
+          alert('Permission denied. Please refresh and give permission.');
+        }
+
+        console.error('getUserMedia() error: ', error);
+      });
+  } else {
+    alert(
+      'Mobile camera is not supported by browser, or there is no camera detected/connected',
+    );
+  }
+});
+
+function initCameraUI() {
+  video = document.getElementById('video');
+
+  takePhotoButton = document.getElementById('takePhotoButton');
+  toggleFullScreenButton = document.getElementById('toggleFullScreenButton');
+  switchCameraButton = document.getElementById('switchCameraButton');
+
+  // https://developer.mozilla.org/nl/docs/Web/HTML/Element/button
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_button_role
+
+  takePhotoButton.addEventListener('click', function () {
+    takeSnapshotUI();
+    takeSnapshot();
+  });
+
+  // -- fullscreen part
+
+  function fullScreenChange() {
+    if (screenfull.isFullscreen) {
+      toggleFullScreenButton.setAttribute('aria-pressed', true);
+    } else {
+      toggleFullScreenButton.setAttribute('aria-pressed', false);
+    }
+  }
+
+  if (screenfull.isEnabled) {
+    screenfull.on('change', fullScreenChange);
+
+    toggleFullScreenButton.style.display = 'block';
+
+    // set init values
+    fullScreenChange();
+
+    toggleFullScreenButton.addEventListener('click', function () {
+      screenfull.toggle(document.getElementById('container')).then(function () {
+        console.log(
+          'Fullscreen mode: ' +
+            (screenfull.isFullscreen ? 'enabled' : 'disabled'),
+        );
+      });
+    });
+  } else {
+    console.log("iOS doesn't support fullscreen (yet)");
+  }
+
+  // -- switch camera part
+  if (amountOfCameras > 1) {
+    switchCameraButton.style.display = 'block';
+
+    switchCameraButton.addEventListener('click', function () {
+      if (currentFacingMode === 'environment') currentFacingMode = 'user';
+      else currentFacingMode = 'environment';
+
+      // initCameraStream();
+    });
+  }
+
+  // Listen for orientation changes to make sure buttons stay at the side of the
+  // physical (and virtual) buttons (opposite of camera) most of the layout change is done by CSS media queries
+  // https://www.sitepoint.com/introducing-screen-orientation-api/
+  // https://developer.mozilla.org/en-US/docs/Web/API/Screen/orientation
+  window.addEventListener(
+    'orientationchange',
+    function () {
+      // iOS doesn't have screen.orientation, so fallback to window.orientation.
+      // screen.orientation will
+      if (screen.orientation) angle = screen.orientation.angle;
+      else angle = window.orientation;
+
+      var guiControls = document.getElementById('gui_controls').classList;
+      var vidContainer = document.getElementById('vid_container').classList;
+
+      if (angle == 270 || angle == -90) {
+        guiControls.add('left');
+        vidContainer.add('left');
+      } else {
+        if (guiControls.contains('left')) guiControls.remove('left');
+        if (vidContainer.contains('left')) vidContainer.remove('left');
+      }
+
+      //0   portrait-primary
+      //180 portrait-secondary device is down under
+      //90  landscape-primary  buttons at the right
+      //270 landscape-secondary buttons at the left
+    },
+    false,
+  );
+}
